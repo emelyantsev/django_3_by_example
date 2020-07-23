dcb09a18177d54efc9cbcdb2ee153e45daaf95e0
emelyantsev <aleks.twin@gmail.com>
2020-07-23 11:58:09 +0300

    finished chapter 1. added models, views, urls and templates

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,13 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
 class Post(models.Model):
     
     STATUS_CHOICES = (
@@ -23,8 +30,20 @@ class Post(models.Model):
 
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
     
+
+    objects = models.Manager() 
+    published = PublishedManager() 
+
+
+
     class Meta:
         ordering = ('-publish',)
     
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                    args=[self.publish.year,
+                        self.publish.month,
+                        self.publish.day, self.slug])
