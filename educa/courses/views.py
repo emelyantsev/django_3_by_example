5f4aa8df22e5eff9767f202c87600052d5383666
emelyantsev <aleks.twin@gmail.com>
2020-09-09 12:42:34 +0300

    add list and detail view for courses

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 
@@ -17,6 +18,9 @@ from .forms import ModuleFormSet
 
 
 from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
+
+from django.db.models import Count
+from .models import Subject
 
 
 class OwnerMixin(object):
@@ -190,3 +194,29 @@ class ContentOrderView(CsrfExemptMixin,
                        module__course__owner=request.user) \
                        .update(order=order)
         return self.render_json_response({'saved': 'OK'})
+
+
+
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject=None):
+
+        subjects = Subject.objects.annotate(total_courses=Count('courses'))
+        
+        courses = Course.objects.annotate(total_modules=Count('modules'))
+        
+        if subject:
+            subject = get_object_or_404(Subject, slug=subject)
+            courses = courses.filter(subject=subject)
+        
+        return self.render_to_response({'subjects': subjects, 'subject': subject, 'courses': courses})
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
+
+
+    
