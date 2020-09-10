b0d7b8c271588e7943d7bd516c282f7f32c539ca
emelyantsev <aleks.twin@gmail.com>
2020-09-10 12:45:12 +0300

    add view course content for students

--- educa/educa/urls.py
+++ educa/educa/urls.py
@@ -19,6 +19,11 @@ from django.contrib.auth import views as auth_views
 
 from courses.views import CourseListView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+
 urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
@@ -28,3 +33,5 @@ urlpatterns = [
     path('students/', include('students.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
